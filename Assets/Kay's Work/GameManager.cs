7547a8d137cfb9391bff267a5cd6491e6c974a55
Kay desit <desistkayla5@gmail.com>
2025-08-08 18:05:06 -0600

    GameUI & gamemanager

---
+++ Assets/Kay's Work/GameManager.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [Header("UI Panels")]
+    public GameObject mainMenuUI;
+    public GameObject inGameUI;
+    public GameObject gameEndUI;
+
+    [Header("In-Game UI")]
+    public TMP_Text lapTimeText;
+    public TMP_Text lapCountText;
+
+    [Header("Game End UI")]
+    public TMP_Text winLoseText;
+    public TMP_Text finalLapTimeText;
+    public Button restartButton;
+    public Button quitButton;
+
+    private float currentLapTime;
+    private int lapCount;
+    private bool isRacing;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        restartButton.onClick.AddListener(RestartGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    private void Start()
+    {
+        mainMenuUI.SetActive(false);
+        inGameUI.SetActive(false);
+        gameEndUI.SetActive(false);
+
+        SetGameState("MainMenu");
+    }
+
+    private void Update()
+    {
+        if (isRacing)
+        {
+            currentLapTime += Time.deltaTime;
+            UpdateLapTimeDisplay();
+        }
+    }
+
+    public void RegisterLap()
+    {
+        lapCount++;
+        lapCountText.text = $"{lapCount}/4";
+
+        if (lapCount >= 4)
+        {
+            EndGame();
+        }
+    }
+
+    public void SetGameState(string state)
+    {
+        // Hide all UI first
+        mainMenuUI.SetActive(false);
+        inGameUI.SetActive(false);
+        gameEndUI.SetActive(false);
+
+        switch (state)
+        {
+            case "MainMenu":
+                mainMenuUI.SetActive(true);
+                Time.timeScale = 0;
+                break;
+
+            case "InGame":
+                inGameUI.SetActive(true);
+                Time.timeScale = 1;
+                StartNewRace();
+                break;
+
+            case "GameEnd":
+                gameEndUI.SetActive(true);
+                Time.timeScale = 0;
+                finalLapTimeText.text = FormatTime(currentLapTime);
+                break;
+        }
+    }
+
+    private void StartNewRace()
+    {
+        currentLapTime = 0f;
+        lapCount = 0;
+        isRacing = true;
+        UpdateLapTimeDisplay();
+        lapCountText.text = "0/4";
+    }
+
+    private void EndGame()
+    {
+        isRacing = false;
+        SetGameState("GameEnd");
+    }
+
+    private void UpdateLapTimeDisplay()
+    {
+        lapTimeText.text = FormatTime(currentLapTime);
+    }
+
+    private string FormatTime(float time)
+    {
+        return $"{time:00.00}s";
+    }
+
+    public void StartGame() => SetGameState("InGame");
+    public void RestartGame() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    public void QuitGame() => Application.Quit();
+}
